7cbf4fe731366e9954b86d4d99ba240044240e74
glkada <36476534+glkada@users.noreply.github.com>
2023-07-14 13:01:19 +0530

    Setting up repository

---
+++ fixtures.js
@@ -0,0 +1,138 @@
+const base = require("@playwright/test");
+const cp = require("child_process");
+const clientPlaywrightVersion = cp
+  .execSync("npx playwright --version")
+  .toString()
+  .trim()
+  .split(" ")[1];
+const BrowserStackLocal = require("browserstack-local");
+
+// BrowserStack Specific Capabilities.
+const caps = {
+  browser: "chrome",
+  os: "osx",
+  os_version: "catalina",
+  name: "My first playwright test",
+  build: "playwright-build-1",
+  "browserstack.username": process.env.BROWSERSTACK_USERNAME || "YOUR_USERNAME",
+  "browserstack.accessKey":
+    process.env.BROWSERSTACK_ACCESS_KEY || "YOUR_ACCESS_KEY",
+  "browserstack.local": process.env.BROWSERSTACK_LOCAL || false,
+  "client.playwrightVersion": clientPlaywrightVersion,
+};
+
+const device_caps = {
+  osVersion: "13.0",
+  deviceName: "Samsung Galaxy S23", // "Samsung Galaxy S22 Ultra", "Google Pixel 7 Pro", "OnePlus 9", etc.
+  browserName: "chrome",
+  realMobile: "true",
+  name: "My android playwright test",
+  build: "playwright-build-1",
+  "browserstack.username": process.env.BROWSERSTACK_USERNAME || "YOUR_USERNAME",
+  "browserstack.accessKey":
+    process.env.BROWSERSTACK_ACCESS_KEY || "YOUR_ACCESS_KEY",
+};
+
+exports.bsLocal = new BrowserStackLocal.Local();
+
+// replace YOUR_ACCESS_KEY with your key. You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
+exports.BS_LOCAL_ARGS = {
+  key: process.env.BROWSERSTACK_ACCESS_KEY || "YOUR_ACCESS_KEY",
+};
+
+// Patching the capabilities dynamically according to the project name.
+const patchCaps = (name, title) => {
+  let combination = name.split(/@browserstack/)[0];
+  let [browerCaps, osCaps] = combination.split(/:/);
+  let [browser, browser_version] = browerCaps.split(/@/);
+  let osCapsSplit = osCaps.split(/ /);
+  let os = osCapsSplit.shift();
+  let os_version = osCapsSplit.join(" ");
+  caps.browser = browser ? browser : "chrome";
+  caps.browser_version = browser_version ? browser_version : "latest";
+  caps.os = os ? os : "osx";
+  caps.os_version = os_version ? os_version : "catalina";
+  caps.name = title;
+};
+
+const patchMobileCaps = (name, title) => {
+  let combination = name.split(/@browserstack/)[0];
+  let [browerCaps, osCaps] = combination.split(/:/);
+  let [browser, deviceName] = browerCaps.split(/@/);
+  let osCapsSplit = osCaps.split(/ /);
+  let os = osCapsSplit.shift();
+  let osVersion = osCapsSplit.join(" ");
+  caps.browser = browser ? browser : "chrome";
+  caps.deviceName = deviceName ? deviceName : "Samsung Galaxy S22 Ultra";
+  caps.osVersion = osVersion ? osVersion : "12.0";
+  caps.name = title;
+  caps.realMobile = "true";
+};
+
+const isHash = (entity) =>
+  Boolean(entity && typeof entity === "object" && !Array.isArray(entity));
+const nestedKeyValue = (hash, keys) =>
+  keys.reduce((hash, key) => (isHash(hash) ? hash[key] : undefined), hash);
+const isUndefined = (val) => val === undefined || val === null || val === "";
+const evaluateSessionStatus = (status) => {
+  if (!isUndefined(status)) {
+    status = status.toLowerCase();
+  }
+  if (status === "passed") {
+    return "passed";
+  } else if (status === "failed" || status === "timedout") {
+    return "failed";
+  } else {
+    return "";
+  }
+};
+
+exports.test = base.test.extend({
+  page: async ({ page, playwright }, use, testInfo) => {
+    // Use BrowserStack Launched Browser according to capabilities for cross-browser testing.
+    if (testInfo.project.name.match(/browserstack/)) {
+      let vBrowser, vContext, vDevice;
+      const isMobile = testInfo.project.name.match(/browserstack-mobile/);
+      if (isMobile) {
+        patchMobileCaps(
+          testInfo.project.name,
+          `${testInfo.file} - ${testInfo.title}`
+        );
+        vDevice = await playwright._android.connect(
+          `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(
+            JSON.stringify(device_caps)
+          )}`
+        );
+        await vDevice.shell("am force-stop com.android.chrome");
+        vContext = await vDevice.launchBrowser();
+      } else {
+        patchCaps(testInfo.project.name, `${testInfo.title}`);
+        vBrowser = await playwright.chromium.connect({
+          wsEndpoint:
+            `wss://cdp.browserstack.com/playwright?caps=` +
+            `${encodeURIComponent(JSON.stringify(caps))}`,
+        });
+        vContext = await vBrowser.newContext(testInfo.project.use);
+      }
+      const vPage = await vContext.newPage();
+      await use(vPage);
+      const testResult = {
+        action: "setSessionStatus",
+        arguments: {
+          status: evaluateSessionStatus(testInfo.status),
+          reason: nestedKeyValue(testInfo, ["error", "message"]),
+        },
+      };
+      await vPage.evaluate(() => {},
+      `browserstack_executor: ${JSON.stringify(testResult)}`);
+      await vPage.close();
+      if (isMobile) {
+        await vDevice.close();
+      } else {
+        await vBrowser.close();
+      }
+    } else {
+      use(page);
+    }
+  },
+});
